694b448f3a484ec435ae080ff6e9d7b01253e934
jqblack <klok111520@email.com>
2021-04-11 12:10:06 -0400

    First commit

---
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -0,0 +1,27 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@SpringBootApplication
+class DemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DemoApplication, args)
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        }
+    }
+}
